afdb4d2749a538dd29f1bd1b70a78726564acfae
phantom <andryone2005@gmail.com>
2026-03-11 00:36:10 +0100

    UPDATE - 10/03/2026
    assunzione coatta da parte del gruppo marino

--- src/config.ts
+++ src/config.ts
@@ -11,75 +11,53 @@ export const siteConfig = {
     github: "https://github.com/oneSpecter",
   },
   aboutMe:
-    "Sono Andrea Blasetti, appassionato di informatica e tecnologie, con un background che unisce competenze tecniche e capacità relazionali. Attualmente studio Informatica presso La Sapienza, dove coltivo il mio interesse per la programmazione e l’innovazione digitale. Parallelamente, ho maturato esperienze lavorative in contesti diversi: dalla gestione completa di un locale come addetto alla ristorazione, al ruolo di assistente bagnante, fino a servizi di babysitting e tutoraggio privato. Questi percorsi mi hanno permesso di sviluppare organizzazione, problem solving, capacità di adattamento e comunicazione efficace. Lo sport ha avuto un ruolo fondamentale nella mia formazione personale: il calcio e, soprattutto, cinque anni di pallanuoto agonistica mi hanno insegnato disciplina, lavoro di squadra e resilienza. Mi considero una persona curiosa, veloce nell’apprendere e motivata a crescere, sempre pronta a mettersi in gioco per raggiungere obiettivi ambiziosi.",
+    "ICT Specialist – Analyst presso Mare Group, dove contribuisco allo sviluppo e all’analisi di soluzioni informatiche in un contesto aziendale strutturato. Parallelamente studio Informatica presso l’Università La Sapienza di Roma, approfondendo programmazione e tecnologie digitali con un forte orientamento all’innovazione e alla crescita continua.",
   skills: ["Java", "Angular", "JavaScript", "Python", "MongoDB", "mySQL", "REST API"],
   projects: [
     {
-      name: "Harmoninq",
-      description:
-      "Applicazione web full-stack per la gestione della libreria musicale personale, con funzionalità di creazione di playlist, caricamento di brani e integrazione con un convertitore da YouTube. Realizzata con Node.js e MongoDB. *Progetto attualmente in fase di sviluppo*",
-      link: "https://github.com/oneSpecter/harmoninq.git",
-      skills: ["Node.js", "Python", "REST API", "User Authentication", "Testing", "Full-stack Development"]
+      name: "MareBank",
+      description: "Progettazione e sviluppo di un’applicazione bancaria web full-stack basata su Java (Spring Boot), Angular e MySQL. L’applicazione implementa un’architettura client-server con API REST per la gestione di utenti, conti bancari, carte e transazioni. Il sistema utilizza un database relazionale strutturato e segue principi di sviluppo software modulari e scalabili, con separazione tra backend e frontend. Il progetto è stato realizzato come dimostrazione pratica di competenze nello sviluppo di applicazioni enterprise e sistemi struttati.",
+      link: "https://github.com/oneSpecter/marebank",
+      skills: ["Angular", "Api Rest", "Java SpringBoot", "MySql"]
     },
     {
       name: "Spiccibot",
       description:
-        "SpicciBot è un bot Telegram per la gestione delle spese e delle entrate personali. Permette agli utenti di registrare movimenti, visualizzare resoconti e gestire preferiti (movimenti ricorrenti).",
+        "SpicciBot è un bot Telegram progettato per la gestione delle finanze personali, sviluppato per semplificare il tracciamento di spese ed entrate direttamente tramite chat. L’applicazione consente agli utenti di registrare movimenti finanziari, generare resoconti e gestire transazioni ricorrenti tramite un sistema di “preferiti”. Il progetto è stato strutturato con logica modulare e orientata all’automazione, con l’obiettivo di offrire un’interfaccia semplice ma efficace per il monitoraggio delle proprie finanze.",
       link: "https://github.com/oneSpecter/SpicciBot.git",
       skills: ["Python", "Telegram Bot API", "SQLite", "Data Visualization"],
     },
     {
       name: "Be Cyber Project",
       description:
-        "Ho creato e gestito un canale YouTube dedicato alla tecnologia, raggiungendo 560 iscritti e 272.709 visualizzazioni. Ho curato l’ideazione, la produzione, il montaggio e la promozione dei video, ottimizzando i contenuti per l’algoritmo di YouTube e interagendo con la community.",
+        "Ideazione, sviluppo e gestione di un canale YouTube dedicato alla tecnologia, con una crescita organica fino a 560 iscritti e oltre 272.000 visualizzazioni. Responsabile dell’intero ciclo di produzione dei contenuti: pianificazione editoriale, registrazione, montaggio e pubblicazione. Ottimizzazione dei video per l’algoritmo della piattaforma attraverso strategie SEO, analisi delle performance e gestione attività.",
       link: "https://www.youtube.com/@becyber",
       skills: ["Content Creation", "Video Editing", "YouTube SEO", "Social Media Management", "Storytelling"]
     },
   ],
   experience: [
     {
-      company: "Belloverde Srl",
-      title: "Cameriere e Barman",
-      dateRange: "Apr 2025 - oggi",
+      company: "Mare Group",
+      title: "ICT Specialist – Analyst",
+      dateRange: "Marzo 2026 – Presente",
       bullets: [
-        "Gestione del servizio ai clienti con approccio orientato alla qualità e all'efficienza",
-        "Preparazione professionale di cocktail e bevande, con attenzione ai dettagli e alla presentazione",
-        "Coordinamento costante con la cucina per garantire tempi di servizio ottimali e comunicazione efficace",
-        "Supporto nella gestione operativa del locale, sviluppando capacità organizzative e problem solving in ambienti dinamici",
+        "Analisi e supporto allo sviluppo di soluzioni software e sistemi informatici in contesto aziendale",
+        "Collaborazione con team tecnici per la progettazione, implementazione e ottimizzazione di applicazioni e servizi IT",
+        "Attività di analisi dei requisiti, supporto ai processi di sviluppo",
       ],
     },
     {
-      company: "Winebar Parsifal – Roma, Italia",
-      title: "Cameriere e Responsabile di Sala",
-      dateRange: "2022 – 2024",
+      company: "Mare Group",
+      title: "Stagista",
+      dateRange: "Febbraio 2026 – Marzo 2026",
       bullets: [
-        "Gestione operativa del locale e coordinamento del personale durante le ore di punta",
-        "Organizzazione e ottimizzazione dei flussi di lavoro per migliorare efficienza e tempi di servizio",
-        "Gestione dei rapporti con i clienti, problem solving e capacità di lavorare sotto pressione",
-        "Applicazione di un approccio sistematico alla risoluzione dei problemi e al miglioramento continuo del servizio",
+        "Supporto alle attività di sviluppo e analisi software all’interno di un ambiente aziendale strutturato",
+        "Affiancamento al team IT nella comprensione dei flussi di lavoro, delle architetture applicative e dei processi di sviluppo",
+        "Partecipazione a task tecnici e attività di testing per il miglioramento e la validazione delle soluzioni software",
       ],
-    },
-    {
-      company: "ASD Roma 2020",
-      title: "Atleta di Pallanuoto Agonista",
-      dateRange: "2018 – 2025",
-      bullets: [
-        "Allenamenti regolari e partecipazione a competizioni in contesti altamente competitivi",
-        "Sviluppo di disciplina, concentrazione e resilienza",
-        "Collaborazione costante in team per il raggiungimento di obiettivi comuni",
-      ],
-    },
+    }
   ],
   education: [
-    {
-      school: "Diploma di Maturità Scientifica",
-      degree: "Liceo Scientifico Pacinotti-Archimede",
-      dateRange: "2019-2024",
-      achievements: [
-        "Diploma con voto 79/100",
-        "Studente atleta con media voto 8/10",
-      ],
-    },
     {
       school: "Università Sapienza di Roma",
       degree: "Laurea in Informatica (in corso)",
@@ -95,8 +73,17 @@ export const siteConfig = {
       dateRange: "04/2025 - 12/2025",
       achievements: [
         "570 ore di corso",
-        //"Esame finale con voto X/30",
+        "Esame finale con voto 30/30",
         "Sviluppo web Java + Angular",
+      ],
+    },
+    {
+      school: "Diploma di Maturità Scientifica",
+      degree: "Liceo Scientifico Pacinotti-Archimede",
+      dateRange: "2019-2024",
+      achievements: [
+        "Diploma con voto 79/100",
+        "Studente atleta con media voto 8/10",
       ],
     },
   ],
